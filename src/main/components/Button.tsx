e0b9c32b87fbb9ba9d87db0b7da225f709023124
Brian Spinos <brianspinos@Brians-MacBook-Pro.local>
2025-01-13 13:41:31 -0800

    add material-UI

---
+++ src/main/components/Button.tsx
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+
+function MyButton() {
+  return (
+    <div>
+      <h1>Welcome to Material-UI</h1>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+    </div>
+  );
+}
+
+export default MyButton;
