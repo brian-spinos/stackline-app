a545936e8e1de4b2759c8d6a478697e42b9d0494
Brian Spinos <brianspinos@Brians-MacBook-Pro.local>
2025-01-13 18:53:14 -0800

    add stackline logo svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import stacklineLogo from "/stackline_logo.svg";
 import MyButton from "./main/components/Button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
         <h1>Stackline</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+
+        <a href="/" target="_blank">
+          <img
+            src={stacklineLogo}
+            className="logo stackline"
+            alt="Stackline logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card"></div>
 
       <MyButton />
     </>
