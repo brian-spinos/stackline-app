dc7b41313a22fd6434d024e9fc1fcacaf2b17026
Brian Spinos <brianspinos@Brians-MacBook-Pro.local>
2025-01-13 17:38:40 -0800

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Stackline!!</h1>
+        <h1>Stackline</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
